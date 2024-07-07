eb3cce8edae250e484b4b776492f271a02bfef2f
tAke <94477820+0xtAke@users.noreply.github.com>
2024-07-07 04:03:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     token: 'MTE1MDc2OTY4NzU0Njg0MzIzNg.GTe6-o.N8qUxBAmGvYCSjD8d0jauWAW5paa_fweLZCMdM',
     channelId: '1150770151285850132',
     embedColor: '#2b2d31',
-    embedFooter: 'RandomPfp by 93cz'
+    embedFooter: 'RandomPfp by tAke'
 };
